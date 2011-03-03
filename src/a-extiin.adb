3df5cb037df02ac354c94318fcf410e2a4ab4795
Kristoffer Nyborg Gregertsen <gregerts@itk.ntnu.no>
2011-03-03 13:01:32 +0100

    Fixed bug with execution time of unused interrupts

--- src/a-extiin.adb
+++ src/a-extiin.adb
@@ -3,9 +3,14 @@ package body Ada.Execution_Time.Interrupts is
    package TMU  renames System.BB.TMU;
 
    function Clock (I : Ada.Interrupts.Interrupt_ID) return CPU_Time is
+      use type TMU.Clock_Id;
+      C : constant TMU.Clock_Id := TMU.Interrupt_Clock (TMU.Interrupt_ID (I));
    begin
-      return CPU_Time
-        (TMU.Execution_Time (TMU.Interrupt_Clock (TMU.Interrupt_ID (I))));
+      if C = null then
+         return CPU_Time_First;
+      else
+         return CPU_Time (TMU.Execution_Time (C));
+      end if;
    end Clock;
 
    function Supported (I : Ada.Interrupts.Interrupt_ID) return Boolean is
