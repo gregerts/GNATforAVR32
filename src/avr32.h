7cef3ed943c702173ba8b6536e815caef4d69ad6
Kristoffer Nyborg Gregertsen <gregerts@itk.ntnu.no>
2011-05-06 15:05:06 +0200

    Updates to context switch routine

--- src/avr32.h
+++ src/avr32.h
@@ -7,6 +7,13 @@
 #define CONTEXT_OFFSET	  0
 #define CONTEXT_SIZE      60
 
+/* TMU registers */
+#define TMU_SWAP_COMPARE    0xFFFF3A10
+#define THREAD_ACTIVE_CLOCK CONTEXT_SIZE
+#define CLOCK_FIRST_ALARM   8
+#define CLOCK_COUNT         0
+#define ALARM_TIMEOUT       0
+
 /* System registers */
 #define SYSREG_SR	  0
 #define SYSREG_EVBA	  4
