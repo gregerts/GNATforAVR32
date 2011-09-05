a08240fb758d735d527e6e3581c08e4d4f9e8805
Kristoffer Nyborg Gregertsen <gregerts@itk.ntnu.no>
2011-09-05 10:21:40 +0200

    Updated NFR-0 again

--- tests/nfr-0/data/test.m
+++ tests/nfr-0/data/test.m
@@ -5,5 +5,14 @@ data = receive(10000);
 i = bitxor(data(:,1),data(:,2)) == data(:,3);
 dt = data(i,2) - data(i,1);
 
-sd = [min(dt); max(dt); mean(dt); sqrt(var(dt))];
+mm = mean(dt);
+iu = find(dt <= mm);
+io = find(dt > mm);
+
+sd = [min(dt(iu)); max(dt(iu)); mean(dt(iu)); sqrt(var(dt(iu)));
+      min(dt(io)); max(dt(io)); mean(dt(io)); sqrt(var(dt(io)))];
+
 printf("N & %d & %d & %1.4f & %1.4f \\\\\n",sd);
+
+fn = sprintf("%s-%s.dat", date, get_branch);
+save(fn, "data", "dt", "sd");
